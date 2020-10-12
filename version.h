6ec685b8606e3d9e9f657edfdb4b9f183d18d12d
Enno Rehling <enno.rehling@gmail.com>
2020-10-13 01:33:31 +0200

    upgrade version number

--- version.h
+++ version.h
@@ -13,17 +13,17 @@
 #define CSMAP_APP_TITLE      "Coast'n'Sea Mapper"
 
 	// current application version and subversion revision
-#define CSMAP_APP_VERSION    0, 5, 1, CSMAP_SVN_REV
+#define CSMAP_APP_VERSION    0, 6, 1, CSMAP_SVN_REV
 #define CSMAP_APP_REVISION   "rev " CSMAP_SVN_REVSTR
 
 	// application version string
-#define CSMAP_APP_VERSIONSTR "v0.5 " CSMAP_APP_REVISION
+#define CSMAP_APP_VERSIONSTR "v0.6 " CSMAP_APP_REVISION
 
 	// application title with full version string
 #define CSMAP_APP_TITLE_VERSION CSMAP_APP_TITLE " " CSMAP_APP_VERSIONSTR
 
 	// used as legal copyright (version.rc) and command line welcome text (main.cpp)
-#define CSMAP_APP_COPYRIGHT  "Copyright (c) 2004-2010 Thomas J. Gritzan"
+#define CSMAP_APP_COPYRIGHT  "Copyright (c) 2004-2020 Thomas J. Gritzan"
 	// used as company name (version.rc) and in the About Box (csmap.cpp)
 #define CSMAP_APP_URL        "http://eressea.draig.de/csmapfx"
 #define CSMAP_APP_EMAIL      "Phygon@gmx.de"
