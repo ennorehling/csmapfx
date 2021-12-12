969efb19aeaf19246b60b2863bf7023b36e8b184
Enno Rehling <enno@eressea.de>
2021-12-12 18:58:44 +0100

    update changelog and version

--- version.h
+++ version.h
@@ -8,10 +8,10 @@
 #define CSMAP_APP_TITLE      "Coast'n'Sea Mapper"
 
 	// current application version and subversion revision
-#define CSMAP_APP_VERSION    0, 9, 4
+#define CSMAP_APP_VERSION    0, 9, 5
 
 	// application version string
-#define CSMAP_APP_VERSIONSTR "v0.9.4"
+#define CSMAP_APP_VERSIONSTR "v0.9.5"
 
 	// application title with full version string
 #define CSMAP_APP_TITLE_VERSION CSMAP_APP_TITLE " " CSMAP_APP_VERSIONSTR
