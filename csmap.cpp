72c4d549f83804b202f6c345fc62e66ad3ebba2b
Enno Rehling <enno@eressea.de>
2023-05-18 21:42:05 +0200

    restore the old context menus

--- csmap.cpp
+++ csmap.cpp
@@ -2407,31 +2407,8 @@ void CSMap::addClipboardPane(FXMenuPane *pane, datablock * block)
     datablock* parent = nullptr;
     std::vector<clip_t> clips;
     block_type type = block->type();
-    FXString label;
-    switch (type) {
-    case block_type::TYPE_UNIT:
-        label = "Einheit";
-/*
-        if (report->getUnit(match, block->info())) {
-            datablock::itor p;
-            if (report->getParent(p, match)) {
-                parent = &*p;
-            }
-        }
-*/
-        break;
-    case block_type::TYPE_SHIP:
-    case block_type::TYPE_BUILDING:
-        label = block->value(TYPE_TYPE);
-        break;
-    case block_type::TYPE_REGION:
-        label = "Region";
-        break;
-    default:
-        label = "Zwischenablage";
-    }
-    new FXMenuSeparatorEx(pane, label);
-
+    new FXMenuSeparatorEx(pane, "Zwischenablage");
+    
     FXString str = block->getName();
     if (!str.empty()) {
         clips.push_back(clip_t{ ID_POPUP_COPY_NAME, str + "\t\tName", block });
