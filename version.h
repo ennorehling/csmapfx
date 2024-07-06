a245731fd43a2c6ebd7c8a645aba1e25deca0565
Enno Rehling <enno@eressea.de>
2024-07-06 11:31:04 +0200

    bugfix: "Einheit hat keinen Befehlsblock"

--- version.h
+++ version.h
@@ -7,10 +7,10 @@
 #define CSMAP_APP_TITLE      "Coast'n'Sea Mapper"
 
 // current application version
-#define CSMAP_APP_VERSION    1, 7, 34
+#define CSMAP_APP_VERSION    1, 7, 35
 
 // application version string
-#define CSMAP_APP_VERSIONSTR "v1.7.34"
+#define CSMAP_APP_VERSIONSTR "v1.7.35"
 
 // application title with full version string
 #define CSMAP_APP_TITLE_VERSION CSMAP_APP_TITLE " " CSMAP_APP_VERSIONSTR
