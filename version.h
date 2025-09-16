f8033c715a63293d98c9fee0056a5b5ea03b3f61
Enno Rehling <enno@eressea.de>
2025-09-16 19:26:50 +0200

    show guarding units

--- version.h
+++ version.h
@@ -7,16 +7,16 @@
 #define CSMAP_APP_TITLE      "Coast'n'Sea Mapper"
 
 // current application version
-#define CSMAP_APP_VERSION    1, 7, 42
+#define CSMAP_APP_VERSION    1, 7, 43
 
 // application version string
-#define CSMAP_APP_VERSIONSTR "v1.7.42-dev"
+#define CSMAP_APP_VERSIONSTR "v1.7.43"
 
 // application title with full version string
 #define CSMAP_APP_TITLE_VERSION CSMAP_APP_TITLE " " CSMAP_APP_VERSIONSTR
 
 // used as legal copyright (version.rc) and command line welcome text (main.cpp)
-#define CSMAP_APP_COPYRIGHT  "Copyright (c) 2004-2010 Thomas J. Gritzan\nCopyright (c) 2021-2024 Enno Rehling"
+#define CSMAP_APP_COPYRIGHT  "Copyright (c) 2004-2010 Thomas J. Gritzan\nCopyright (c) 2021-2025 Enno Rehling"
 // used as company name (version.rc) and in the About Box (csmap.cpp)
 #define CSMAP_APP_URL        "https://github.com/ennorehling/csmapfx"
 
